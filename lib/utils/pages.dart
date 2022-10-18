6410eecb66d2ce67be178827f5fbe662f37568f0
Mohammad Hossein Khoshakm <88422929+mkhoshkam@users.noreply.github.com>
2022-10-18 15:13:25 +0330

    #init: page models

---
+++ lib/utils/pages.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+import 'package:puppy_challenge/bindings/homeBinding.dart';
+import 'package:puppy_challenge/bindings/serviceBinding.dart';
+import 'package:puppy_challenge/utils/routes.dart';
+import 'package:puppy_challenge/views/HomeView.dart';
+import 'package:puppy_challenge/views/serviceView.dart';
+
+class Pages{
+  static final pages = <GetPage>[
+    GetPage(name: Routes.home, page:()=> HomeView(),binding: HomeBinding()),
+    GetPage(name: Routes.service, page:()=> const ServiceView(),binding: ServiceBinding())
+  ];
+}
+
