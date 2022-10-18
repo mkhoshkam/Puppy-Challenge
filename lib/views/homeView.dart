9962bf061dfdb9498cf79a4f002545b9c0454be6
Mohammad Hossein Khoshakm <88422929+mkhoshkam@users.noreply.github.com>
2022-10-18 15:11:53 +0330

    #init: home screen

---
+++ lib/views/homeView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:puppy_challenge/controllers/homeController.dart';
+
+class HomeView extends GetView<HomeController>{
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
