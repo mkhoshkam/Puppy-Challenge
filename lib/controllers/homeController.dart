05ee39d23e422a417e9cb91598866a9993050759
Mohammad Hossein Khoshakm <88422929+mkhoshkam@users.noreply.github.com>
2022-10-19 11:45:58 +0330

    #feat: home page

--- lib/controllers/homeController.dart
+++ lib/controllers/homeController.dart
@@ -1,5 +1,97 @@
 import 'package:get/get.dart';
+import 'package:puppy_challenge/interfaces/homeInterface.dart';
+import 'package:puppy_challenge/models/request/animalRequestModel.dart';
+import 'package:puppy_challenge/models/request/calculateRequestModel.dart';
+import 'package:puppy_challenge/repositories/calculateRepository.dart';
 
-class HomeController extends GetxController{
+import '../interfaces/catInterface.dart';
+import '../interfaces/dogInterface.dart';
+import '../models/categoryModel.dart';
+import '../models/request/hotelRequestModel.dart';
+import '../models/request/servicesRequestModel.dart';
+import '../models/response/responseCalculate.dart';
+import '../network/calculateProvider.dart';
+import '../utils/routes.dart';
 
+class HomeController extends GetxController implements HomeInterface,CatInterface,DogInterface {
+  RxInt calculatedAmount = 0.obs;
+  RxBool isCalculating = false.obs;
+  late CalculatorRequest calculatorRequest;
+  late CalculateRepository calculateRepository;
+
+  @override
+  void onInit() {
+    initializeRepositories();
+    initializeRequest();
+    super.onInit();
+  }
+
+  @override
+  void calculate() {
+    calculateIsLoading(true);
+    calculateRepository.calculateFromServer(calculatorRequest.toJson())
+        .then((response){
+      calculateIsLoading(false);
+          if(response.status.isOk){
+            try{
+              calculatedAmount.value = responseCalculateFromJson(response.bodyString.toString()).totalPrice!.toInt();
+            }catch(_){
+             calculateIsLoading(false);
+            }
+          }
+    })
+        .onError((error, stackTrace) {});
+  }
+
+  @override
+  void selectCategory(CategoryModel categoryModel) {
+    categoryModel.animal = categoryModel.name == "cat" ? calculatorRequest.cat : calculatorRequest.dog;
+    Get.toNamed(Routes.service, arguments: {'model': categoryModel})
+        ?.then((value) {
+          try{
+            Animal animal =value['data'] as Animal;
+            if(categoryModel.name == "cat"){
+              calculatorRequest.cat = animal;
+            }else{
+              calculatorRequest.dog = animal;
+            }
+          }catch(_){}
+    });
+  }
+
+  @override
+  void initializeRequest() {
+    Animal animal = initializeAnimalModel();
+    calculatorRequest = CalculatorRequest(cat: animal,dog: animal);
+  }
+
+  Animal initializeAnimalModel(){
+    Hotel hotel = Hotel(nights: 0);
+    Services services = Services(hotel: hotel,grooming: false);
+    Animal animal = Animal(services: services);
+    return animal;
+  }
+
+  @override
+  void calculateIsLoading(bool state) => isCalculating.value = state;
+
+  @override
+  void initializeRepositories() => calculateRepository = CalculateRepository();
+
+  @override
+  void resetFilter(CategoryModel categoryModel) {
+    categoryModel.name == "cat" ? resetCatFilters():resetDogFilters();
+  }
+
+  @override
+  void resetCatFilters() {
+    Animal animal = initializeAnimalModel();
+    calculatorRequest.cat = animal;
+  }
+
+  @override
+  void resetDogFilters() {
+    Animal animal = initializeAnimalModel();
+    calculatorRequest.dog = animal;
+  }
 }
--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:puppy_challenge/controllers/homeController.dart';
+import 'package:puppy_challenge/widgets/appBar.dart';
+import 'package:puppy_challenge/widgets/categoryItem.dart';
 
-class HomeView extends GetView<HomeController>{
+import '../utils/faker.dart';
+import '../utils/theme.dart';
+
+class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+        appBar: appBar("Home", canPop: false),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [Expanded(child: categories()), calculateLayout()],
+          ),
+        ));
+  }
+  Widget categories() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: categoriesData.map((category) =>
+          categoryItem(
+              category,
+              selectCategory: controller.selectCategory,
+            resetFilter: controller.resetFilter
+          )
+      ).toList(),
+    );
   }
 
+  Widget calculateLayout() {
+    return Container(
+      padding: const EdgeInsets.all(12.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(cardRadius),
+      ),
+      child: Obx(() => Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              controller.isCalculating.value
+                  ? const CircularProgressIndicator()
+                  : ElevatedButton(
+                      onPressed: controller.calculate,
+                      child: const Text("Calculate", style: buttonTextStyle)),
+              Text("${controller.calculatedAmount.value} \$")
+            ],
+          )),
+    );
+  }
 }
