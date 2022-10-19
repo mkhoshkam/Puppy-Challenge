093857031b4e0343c30121cea362c56d9ed03e3b
Mohammad Hossein Khoshakm <88422929+mkhoshkam@users.noreply.github.com>
2022-10-19 11:45:45 +0330

    #feat: service page

--- lib/controllers/serviceController.dart
+++ lib/controllers/serviceController.dart
@@ -1,5 +1,48 @@
 import 'package:get/get.dart';
+import 'package:puppy_challenge/interfaces/serviceInterface.dart';
+import 'package:puppy_challenge/models/categoryModel.dart';
+import 'package:puppy_challenge/models/request/animalRequestModel.dart';
+import 'package:puppy_challenge/models/request/hotelRequestModel.dart';
+import 'package:puppy_challenge/models/request/servicesRequestModel.dart';
 
-class ServiceController extends GetxController{
+class ServiceController extends GetxController implements ServiceInterface{
+  late CategoryModel categoryModel;
 
+  RxBool hasGrooming = false.obs;
+  RxBool hasHotel = false.obs;
+  RxInt nightOfHotel = 1.obs;
+
+  @override
+  void onInit() {
+    categoryModel = Get.arguments['model'];
+    initialData();
+    super.onInit();
+  }
+
+  @override
+  void initialData() {
+    int nights = categoryModel.animal!.services!.hotel!.nights!.toInt();
+    hasGrooming.value = categoryModel.animal!.services!.grooming!;
+    nightOfHotel.value =  nights == 0 ? 1 : nights;
+    hasHotel.value =  nights != 0;
+  }
+
+  @override
+  void submit() {
+    Hotel hotel = Hotel(nights: hasHotel.value ? nightOfHotel.value:0);
+    Services services = Services(hotel: hotel,grooming: hasGrooming.value);
+    Animal animal = Animal(services: services);
+    Get.back(result: {"data":animal});
+  }
+
+  @override
+  void addHotelNight() => nightOfHotel.value++;
+
+
+  @override
+  void minusHotelNight() {
+    if(nightOfHotel.value > 1){
+      nightOfHotel.value --;
+    }
+  }
 }
--- lib/views/serviceView.dart
+++ lib/views/serviceView.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:puppy_challenge/controllers/serviceController.dart';
+import 'package:puppy_challenge/widgets/appBar.dart';
+import 'package:puppy_challenge/widgets/categoryItem.dart';
+
+import '../utils/theme.dart';
 
 class ServiceView extends GetView<ServiceController>{
   const ServiceView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+        appBar: appBar("Services"),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              categoryItem(controller.categoryModel),
+              Expanded(child: serviceLayout()),
+              submitButton()
+            ],
+          ),
+        ));
+  }
+
+  Widget serviceLayout() {
+    return Obx(() => Column(
+      children: [
+        CheckboxListTile(title:const Text("Grooming"),value: controller.hasGrooming.value, onChanged: (state){controller.hasGrooming.value = state!;}),
+        CheckboxListTile(title:const Text("Hotel"),value: controller.hasHotel.value, onChanged: (state){controller.hasHotel.value = state!;}),
+        Obx(() => controller.hasHotel.value ? ListTile(title:const Text("Nights"),trailing:nightOfHotelLayout()):Container())
+      ],
+    ));
+  }
+
+  Widget nightOfHotelLayout() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        IconButton(onPressed: controller.minusHotelNight, icon: const Icon(Icons.remove_circle,color: Colors.red)),
+        Obx(() => Text("${controller.nightOfHotel}")),
+        IconButton(onPressed: controller.addHotelNight, icon: const Icon(Icons.add_circle,color: Colors.green,))
+      ],
+    );
+  }
+
+  ElevatedButton submitButton() {
+    return ElevatedButton(
+        onPressed: controller.submit,
+        child:  Row(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+          Icon(Icons.check,color: Colors.orange,),
+          Text("Submit", style: buttonTextStyle)
+        ],));
   }
 
 }
