e516d02da119c821323aef48ad8560d459b7a4d6
Mohammad Hossein Khoshakm <88422929+mkhoshkam@users.noreply.github.com>
2022-10-19 11:41:12 +0330

    #feat: add view themes

---
+++ lib/utils/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final ThemeData themeData = ThemeData(
+  primaryColor: Colors.orange,
+  highlightColor: Colors.orange,
+  progressIndicatorTheme:const ProgressIndicatorThemeData(color: Colors.orange),
+  useMaterial3: true,
+);
+
+const cardRadius = 8.0;
+const buttonTextColor = Colors.orange;
+
+
+const buttonTextStyle =  TextStyle(color: buttonTextColor);
+const filterButtonTextStyle =  TextStyle(color: Colors.red,fontSize: 12);
