8bc0c5b5dea13319e29cbce67004459c607e9b96
Mohammad Hossein Khoshakm <88422929+mkhoshkam@users.noreply.github.com>
2022-10-19 11:45:30 +0330

    #fix: set theme to app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:puppy_challenge/utils/pages.dart';
 import 'package:puppy_challenge/utils/routes.dart';
+import 'package:puppy_challenge/utils/theme.dart';
 
 void main() {
   runApp(app());
@@ -11,6 +12,7 @@ GetMaterialApp app() {
   return GetMaterialApp(
       initialRoute: Routes.home,
       debugShowCheckedModeBanner: false,
+      theme: themeData,
       locale:const Locale('en_US'),
       getPages: Pages.pages);
 }
